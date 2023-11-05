12c20800099f5236f31910ea67c8f3510923d15c
PrakhovEgor <prakhov.egor@gmail.com>
2023-11-05 23:49:07 +0300

    05.11

--- test_app.py
+++ test_app.py
@@ -30,11 +30,16 @@ class TestApp(unittest.TestCase):
         self.assertIsNotNone(element)
 
     def test_transform_period(self):
-        corr_input = [{'id': 2, 'email': 'egor.prakhov@mail.ru', 'name': 'Напоминание', 'm_type': '-', 'tg_id': ['1'], 'plants': [], 'start_date': '2023-11-06T22:00', 'period': '1_day', 'comment': '', 'plants_ids': []}]
-        corr_output = [{'id': 2, 'email': 'egor.prakhov@mail.ru', 'name': 'Напоминание', 'm_type': '-', 'tg_id': ['1'], 'plants': [], 'start_date': '2023-11-06T22:00', 'period': 'каждый день', 'comment': '', 'plants_ids': []}]
+        corr_input = [{'id': 2, 'email': 'egor.prakhov@mail.ru', 'name': 'Напоминание', 'm_type': '-', 'tg_id': ['1'],
+                       'plants': [], 'start_date': '2023-11-06T22:00', 'period': '1_day', 'comment': '',
+                       'plants_ids': []}]
+        corr_output = [{'id': 2, 'email': 'egor.prakhov@mail.ru', 'name': 'Напоминание', 'm_type': '-', 'tg_id': ['1'],
+                        'plants': [], 'start_date': '2023-11-06T22:00', 'period': 'каждый день', 'comment': '',
+                        'plants_ids': []}]
 
         test_output = transform_period(corr_input)
         self.assertEqual(corr_output, test_output)
+
 
 if __name__ == '__main__':
     unittest.main()
